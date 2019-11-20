04d5a9b6e1500daadbb9195902c19bed0429986d
Montano <manuelelp@gmail.com>
2019-11-21 00:00:45 +0100

    Un poco de compactacion de codigo

--- CodeTest.Calculator/Operations/AdditionOperation.cs
+++ CodeTest.Calculator/Operations/AdditionOperation.cs
@@ -4,13 +4,12 @@
     { 
         public override int Calculate(int firstValue, params int[] values)
         {
-            int response = firstValue;
             foreach (var item in values)
             {
-                response += item;
+                firstValue += item;
             }
 
-            return response;
+            return firstValue;
         }
     }
 }
