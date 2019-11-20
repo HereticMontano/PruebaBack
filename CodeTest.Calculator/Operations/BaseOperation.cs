da3e0e4e9799a1332d942ec41035e335888c1ad1
Montano <manuelelp@gmail.com>
2019-11-20 23:52:23 +0100

    Cambio de lugar de comentario

--- CodeTest.Calculator/Operations/BaseOperation.cs
+++ CodeTest.Calculator/Operations/BaseOperation.cs
@@ -4,6 +4,9 @@ namespace CodeTest.Calculator.Operations
 {
     public abstract class BaseOperation : IOperation
     {
+        /* Todas las operaciones van a recivir al menos un parametro obligatorio y podran aceptar 1 o N parametros mas, 
+       * la idea es poder dar soporte para operaciones que necesitan dos o mas operadores para llevarse acabo 
+       * como operaciones mas simples de un solo operador (logaritmos, tangentes, cotangentes, etc)*/
         public abstract int Calculate(int firstValue, params int[] values);
     }
 }
