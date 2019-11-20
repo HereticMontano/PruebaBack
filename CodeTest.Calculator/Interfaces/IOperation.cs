5ae9a613c3ae44f4338c92fa123cfeefd081e737
HereticMontano <manuel.lopez@samelan.com>
2019-11-20 15:47:01 +0100

    Cambio comentario

--- CodeTest.Calculator/Interfaces/IOperation.cs
+++ CodeTest.Calculator/Interfaces/IOperation.cs
@@ -3,9 +3,9 @@ namespace CodeTest.Calculator.Interfaces
 {
     public interface IOperation
     {
-        /* Todas las operaciones van a recivir al menos un parametro obligatorio y podran aceptar 0 o N parametros mas, 
+        /* Todas las operaciones van a recivir al menos un parametro obligatorio y podran aceptar 1 o N parametros mas, 
          * la idea es poder dar soporte para operaciones que necesitan dos o mas operadores para llevarse acabo 
-         * como operaciones mas simples de un solo operador como logaritmos, tangentes, cotangentes, etc)*/
+         * como operaciones mas simples de un solo operador (logaritmos, tangentes, cotangentes, etc)*/
         int Calculate(int firstValue, params int[] values);
     }
 }
